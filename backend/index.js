56d444b6469b8cc2f163dc9bee9cb39318c006cc
OctopuSSX <vladik0701@gmail.com>
2023-08-24 23:39:15 +0300

    Remove console.log

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,6 @@ app.get('/map/:id/:redl', async (req, res) => {
 
 const static = express.static('dist');
 app.get('*', (req, res, next) => {
-  console.log(req.url);
   req.url = (req.url.length > 1 && req.url.endsWith("/")) ? req.url.slice(0, req.url.length - 1) : req.url;
   static(req, res, next);
 });
